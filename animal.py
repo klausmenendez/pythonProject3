1374f199283b57b6a9dade9945bbf779d30f5dea
klausmenendez <114318894+klausmenendez@users.noreply.github.com>
2022-09-28 23:10:06 -0700

    Initial commit

---
+++ animal.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+fav_animal=input("Please enter your favorite animal: ")
+    # Use a breakpoint in the code line below to debug your script.
+print("Your favorite animal is the " + fav_animal + ".")
+
+
+# Press the green button in the gutter to run the script.
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
