07e6c69d8d9dd3041f72382fdc6381128f0bea17
klausmenendez <114318894+klausmenendez@users.noreply.github.com>
2022-09-28 23:27:35 -0700

    Update and rename main.py to animal.py

--- animal.py
+++ animal.py
@@ -4,7 +4,7 @@
 # Date: 9/28/2022
 # Description: Asks the user for their favorite animal and then prints out that that is their favorite animal
 fav_animal=input("Please enter your favorite animal: ")
-    # Use a breakpoint in the code line below to debug your script.
+  
 print("Your favorite animal is the " + fav_animal + ".")
 
 
