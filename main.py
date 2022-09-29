18f6d7846967300d4c257785f537589e0f92be0a
klausmenendez <114318894+klausmenendez@users.noreply.github.com>
2022-09-28 23:16:38 -0700

    commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# author: klaus menendez
+
+# GitHub username: Klaus Menendez
+# Date: 9/28/2022
+# Description: Asks the user for their favorite animal and then prints out that that is their favorite animal
+fav_animal=input("Please enter your favorite animal: ")
+    # Use a breakpoint in the code line below to debug your script.
+print("Your favorite animal is the " + fav_animal + ".")
+
+
+
